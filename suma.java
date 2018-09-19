b7142b2e2030a03e72ce321c0984cc98fc67c720
Abdel Mouden <moudeen8@gmail.com>
2018-09-19 18:41:21 +0200

    Modificat en angles
    Sa modificat a l'angles

---
+++ suma.java
@@ -0,0 +1,9 @@
+
+int numero1 = 0;
+int numero2 = 0;
+
+System.out.println("Send first number: ");
+numero1 = reader.nextInt();
+
+System.out.println("Send second number: ");
+numero2 = reader.nextInt();
